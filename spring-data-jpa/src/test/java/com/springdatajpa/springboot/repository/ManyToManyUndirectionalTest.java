ed75def5ef7ae453a0e8099bee52320e653072ad
Tran Nguyen <16131104@student.hcmute.edu.vn>
2022-05-20 20:28:23 +0700

    feature/fix conflict

---
+++ spring-data-jpa/src/test/java/com/springdatajpa/springboot/repository/ManyToManyUndirectionalTest.java
@@ -0,0 +1,62 @@
+package com.springdatajpa.springboot.repository;
+
+import com.springdatajpa.springboot.entity.Role;
+import com.springdatajpa.springboot.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ManyToManyUndirectionalTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void saveUser(){
+        User user = new User();
+        user.setFirstName("Tran");
+        user.setLastName("Nguyen");
+        user.setEmail("tranntn260@gmail.com");
+        user.setPassword("secrete");
+
+        Role admin = new Role();
+        admin.setName("ROLE_ADMIN");
+
+        Role customer = new Role();
+        customer.setName("ROLE_CUSTOMER");
+
+        user.getRoles().add(admin);
+        user.getRoles().add(customer);
+
+        userRepository.save(user);
+    }
+
+    @Test
+    void updateUser(){
+        User user = userRepository.findById(1L).get();
+        user.setFirstName("Tung");
+        user.setLastName("Bui");
+        user.setEmail("tungbui@gmail.com");
+
+        Role roleUser = new Role();
+        roleUser.setName("ROLE_USER");
+
+        user.getRoles().add(roleUser);
+
+        userRepository.save(user);
+    }
+
+    @Test
+    void fetchUser(){
+        User user = userRepository.findById(1L).get();
+        System.out.println(user.getEmail());
+        user.getRoles().forEach(role -> {
+            System.out.println(role.getName());
+        });
+    }
+
+    @Test
+    void deleteUser(){
+        userRepository.deleteById(2L);
+    }
+}
